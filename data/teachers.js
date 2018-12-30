ec0ba04c3e3e73611b5192fdea1731e10cdb9164
Davis JR <davisjr2000@hotmail.com>
2018-12-30 00:44:26 -0200

    Add fake teachers data

---
+++ data/teachers.js
@@ -0,0 +1,35 @@
+const teachers = [
+  {
+    name: "Davis JR",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/davisjr2000",
+    specialty: "React",
+  },
+  {
+    name: "Anderson Chen",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/anderchen",
+    specialty: "Ruby",
+  },
+  {
+    name: "Ana Riccetti",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/anariccetti",
+    specialty: "CSS",
+  },
+  {
+    name: "Gabriel Schiavo",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/gbs0",
+    specialty: "JavaScript",
+  },
+  {
+    name: "Thierry Silvagnoli",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/ThierryMR",
+    specialty: "Bitcoin",
+  },
+  {
+    name: "Martinho Hoffman",
+    imageUrl: "https://kitt.lewagon.com/placeholder/users/martinhohoff",
+    specialty: "Rails",
+  },
+
+];
+
+export default teachers;
